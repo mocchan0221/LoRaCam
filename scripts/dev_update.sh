b7ba9fa7581eacb33f26fb9a32f343ca06c522c0
mocchan0221 <sakahiro230221@gmail.com>
2026-01-14 20:23:31 +0900

    Modify update script

--- scripts/dev_update.sh
+++ scripts/dev_update.sh
@@ -15,10 +15,13 @@ $PROJECT_DIR/.venv/bin/pip install -r requirements.txt
 
 echo "サービスファイルの更新中"
 sudo cp $PROJECT_DIR/services/LoRaCam.service /etc/systemd/system
+sudo cp $PROJECT_DIR/services/WebMonitor.service /etc/systemd/system
 
 echo "サービスを再起動中"
-sudo sytemctl daemon-reload
+sudo systemctl daemon-reload
 sudo systemctl restart LoRaCam.service
+sudo systemctl restart WebMonitor.service
 
 echo "更新が完了しました"
 sudo systemctl status LoRaCam.service --no-pager
+sudo systemctl status WebMonitor.service --no-pager
