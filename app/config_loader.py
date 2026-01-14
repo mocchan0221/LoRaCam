2a16f20f2a7518079300c9b573019900913f5b83
mocchan0221 <sakahiro230221@gmail.com>
2026-01-14 19:58:53 +0900

    add config loading log

--- app/config_loader.py
+++ app/config_loader.py
@@ -11,18 +11,22 @@ CONFIG_PATHS = [
 DEFAULT_CONFIG_PATH = "/home/jkkb/LoRaCam/config_template.json"            
 
 def load_config():
+    print("Config Loading...")
     # 設定ファイルの存在を確認
     config_path = None
     for path in CONFIG_PATHS:
         if os.path.exists(path):
+            print("Config file found !")
             config_path = path
             break
 
     try:
         with open(config_path, 'r') as f:
             config = json.load(f)
+            print("Config file correctly Loaded !")
             return config
     except Exception as e:
         with open(DEFAULT_CONFIG_PATH, 'r') as f:
             config = json.load(f)
+            print("Failed to load config, using default")
             return config
